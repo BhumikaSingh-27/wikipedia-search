665a78dd75c529ad0a17d94d8647016b68d4129c
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-09 13:41:12 +0530

    feat : added the search functionality
    added JavaScript code in app.js for search functionality implementation

---
+++ app.js
@@ -0,0 +1,43 @@
+const formData = document.querySelector("#form-data");
+const userInput = document.querySelector("#user-input");
+const searchBtn = document.querySelector("#get-result");
+const output = document.querySelector("#output");
+
+const input = userInput.value;
+function getURL(text) {
+  let serverURL = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${text}`;
+  //   console.log(serverURL);
+  return serverURL;
+}
+
+function display(result) {
+  output.innerHTML = " ";
+  output.insertAdjacentHTML("beforeend", `Search results for ${input}`);
+  result.forEach((item) => {
+    let title = item.title;
+    let snippet = item.snippet;
+    let itemURL = encodeURI(`https://en.wikipedia.org/wiki/${title}`);
+    // console.log(title);
+    // console.log(snippet);
+    // console.log(itemURL);
+    output.insertAdjacentHTML(
+      "beforeend",
+      `<div><h2>${title}</h2><h3>${snippet}</h3><p><a href=${itemURL}>${itemURL}</a></p></div>`
+    );
+  });
+
+  //   console.log(result);
+}
+
+function searchHandler() {
+  //   console.log(userInput.value);
+  fetch(getURL(userInput.value))
+    .then((response) => response.json())
+    .then((json) => {
+      let arrayResults = json.query.search;
+      display(arrayResults);
+    })
+    // .then((json) => console.log(json.query.search))
+    .catch((error) => console.log(error));
+}
+searchBtn.addEventListener("click", searchHandler);
